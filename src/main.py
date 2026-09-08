261ee88d12336a5b0e71b7aaceba0316a24d5f42
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-09-08 14:28:32 +0300

    feat: serve compiled frontend from the backend

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
+from pathlib import Path
+
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+STATIC_DIR = Path(__file__).parent / "static"
 
 app = FastAPI()
+
+app.mount("/", StaticFiles(directory=STATIC_DIR, html=True), name="static")
