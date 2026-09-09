5704a095e636e0e0d9b7c784af90ef5eb55b7eb4
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-09-09 09:43:31 +0300

    feat: add mock /users/me endpoint under /frontend-api prefix

--- src/main.py
+++ src/main.py
@@ -1,10 +1,26 @@
 from pathlib import Path
 
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
 from fastapi.staticfiles import StaticFiles
 
 STATIC_DIR = Path(__file__).parent / "static"
 
-app = FastAPI()
+app = FastAPI(docs_url="/frontend-api/docs")
 
+router = APIRouter(tags=["Users"])
+
+
+@router.get("/users/me")
+def get_current_user():
+    return {
+        "email": "example@example.com",
+        "isActive": True,
+        "profileId": "1",
+        "registeredAt": "2025-06-15T18:29:56+00:00",
+        "updatedAt": "2025-06-15T18:29:56+00:00",
+        "username": "user123",
+    }
+
+
+app.include_router(router, prefix="/frontend-api")
 app.mount("/", StaticFiles(directory=STATIC_DIR, html=True), name="static")
